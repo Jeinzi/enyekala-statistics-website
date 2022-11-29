ecda356150767b0e5199cfd760e132188e37c0d4
Jeinzi <jeinzi@gmx.de>
2022-11-29 18:07:55 +0100

    Add link to player page to overview table

--- res/template/table-row-player.php
+++ res/template/table-row-player.php
@@ -1,6 +1,6 @@
 <tr>
   <td><?= $rank ?></td>
-  <td><?= $name ?></td>
+  <td><a href="/player?name=<?= $name ?>" class="no-link-deco"><?= $name ?></a></td>
   <td><?= $firstLogin ?></td>
   <td><?= $nLogins ?></td>
   <td><?= $nMsg ?></td>
